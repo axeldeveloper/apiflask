d8d782fafa088cdd2ed475881bc5e5e4bf6885b6
Axel Alexander <axel_nomore@hotmail.com>
2017-11-26 17:17:21 -0200

    incluindo IDE

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Axel OpenShift Hello World!"
+    return "Conectando Flask SQLAlchemy POSTGRESS!"
 
 if __name__ == "__main__":
     application.run()
